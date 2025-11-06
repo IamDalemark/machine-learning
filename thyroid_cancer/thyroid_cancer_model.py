c6bf4fdccce648668d5f67f6f4bf4f7eecdded8d
Daniel Mark Wiegand <danielweg123@gmail.com>
2025-11-06 16:19:24 +0800

    feat: clean unnecessary text from document with shap

--- thyroid_cancer/thyroid_cancer_model.py
+++ thyroid_cancer/thyroid_cancer_model.py
@@ -4,11 +4,11 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import MaxAbsScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, ENGLISH_STOP_WORDS
 from matplotlib import pyplot as plt
 import seaborn as sns
 import pickle
-
+import shap as sh
 # open
 
 df = pd.read_csv("cancer_dataset.csv")
@@ -19,8 +19,10 @@ def preprocessCancer(df):
     df["cancer_type"] = np.where(df["cancer_type"] == "Thyroid_Cancer", 1, 0)
     return df["cancer_type"]
 
+custom_stopwords = ["fig", "open", "et"]
+stop_words = list(ENGLISH_STOP_WORDS.union(custom_stopwords))
 def preprocessResearch(df):
-    vectorizer = TfidfVectorizer(max_features=5000)
+    vectorizer = TfidfVectorizer(max_features=5000, stop_words=stop_words,  token_pattern=r'\b[a-zA-Z]{2,}\b' )
     tfidf_matrix = vectorizer.fit_transform(df["research_document"])
     return tfidf_matrix, vectorizer
 
@@ -47,13 +49,15 @@ def hypertuneModel(X_train, y_train):
     'solver': ['lbfgs'],
     'max_iter'  : [100]
     }
-    gridsearch = GridSearchCV(model, param_grid=param_grid, n_jobs=-1)
+    gridsearch = GridSearchCV(model, param_grid=param_grid, n_jobs=-1, verbose=2)
     gridsearch.fit(X_train, y_train)
     print(gridsearch.best_params_)
     return gridsearch.best_estimator_
 
 model = hypertuneModel(X_train, Y_train)
 # evaluation
+print("----evaluation----")
+
 
 def evaluate(model, X_test, Y_test):
     prediction = model.predict(X_test)
@@ -68,7 +72,7 @@ print()
 print(accuracy)
 print("----matrix----")
 print(matrix)
-
+print(stop_words)
 # save model ang dump
 
 with open("thyroid_cancer_model.pkl", "wb") as f:
@@ -80,3 +84,8 @@ with open("scaler.pkl", "wb") as f:
 with open("vectorizer.pkl", "wb") as f:
     pickle.dump(vectorizer, f)
 
+explainer = sh.LinearExplainer(model, X, feature_perturbation="interventional")
+shap_values = explainer(X[:200])
+
+feature_names = vectorizer.get_feature_names_out()
+sh.summary_plot(shap_values, X[:200].toarray(), feature_names=feature_names, max_display = 10)
